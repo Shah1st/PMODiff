94caad324a814cc40f2e1d270d45802561de2141
Andrij Vasylenko <andrij@login1.barkla2.liv.alces.network>
2025-12-22 14:18:11 +0000

    Cmpt loss sweep: 0.003 0.01 0.03

--- tuning_100k_spp0_cmpt0.sh
+++ tuning_100k_spp0_cmpt0.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 
-#SBATCH -o slurm%j.out
-#SBATCH -J tune0_0
+#SBATCH -o tune_c003
+#SBATCH -J tune_c003
 #SBATCH -p gpu-a-lowsmall,gpu-l40s,gpu-l40s-low,gpu-h100
 #SBATCH --gres=gpu:1
-#SBATCH -t 8:00:00
+#SBATCH -t 24:00:00
 #SBATCH --mem-per-cpu=8000M
 #STDIN=./infile
 
@@ -14,5 +14,7 @@ conda activate tagmol
 echo $CUDA_VISIBLE_DEVICES
 cd "$SLURM_SUBMIT_DIR"
 cd /mnt/scratch/users/andrij/PMODiff
-#python scripts/train_diffusion.py configs/tuning_0_0.yml
-python -m scripts.train_diffusion --config configs/tuning_0_0.yml
+python -m scripts.train_diffusion --config configs/tuning_c003.yml
+#python -m scripts.train_diffusion --config configs/tuning_0_0.yml
+#python -m scripts.train_diffusion --config configs/tuning_0_0.yml
+#python -m scripts.train_diffusion --config configs/tuning_0_0.yml
---
+++ tuning_100k_spp0_cmpt01.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+#SBATCH -o tune_c01
+#SBATCH -J tune_c01
+#SBATCH -p gpu-a-lowsmall,gpu-l40s,gpu-l40s-low,gpu-h100
+#SBATCH --gres=gpu:1
+#SBATCH -t 24:00:00
+#SBATCH --mem-per-cpu=8000M
+#STDIN=./infile
+
+source ~/.bashrc
+conda activate tagmol
+#conda list
+echo $CUDA_VISIBLE_DEVICES
+cd "$SLURM_SUBMIT_DIR"
+cd /mnt/scratch/users/andrij/PMODiff
+python -m scripts.train_diffusion --config configs/tuning_c01.yml
+#python -m scripts.train_diffusion --config configs/tuning_c003.yml
